63d0575f213c3e4c6ebeeecc3f6c4f96c0321ee0
Adrian Mejias <adrianjmejias@gmail.com>
2020-01-12 15:15:32 -0400

    Change GetItemsLineHeightWithSpacing for GetFrameHeightWithSpacing
    the currently used GetItemsLineHeightWithSpacing is now deprecated.

--- imfilebrowser.h
+++ imfilebrowser.h
@@ -345,10 +345,10 @@ inline void ImGui::FileBrowser::Display()
 
     // browse files in a child window
 
-    float reserveHeight = GetItemsLineHeightWithSpacing();
+    float reserveHeight = GetFrameHeightWithSpacing();
     std::filesystem::path newPwd; bool setNewPwd = false;
     if(!(flags_ & ImGuiFileBrowserFlags_SelectDirectory) && (flags_ & ImGuiFileBrowserFlags_EnterNewFilename))
-        reserveHeight += GetItemsLineHeightWithSpacing();
+        reserveHeight += GetFrameHeightWithSpacing();
     {
         BeginChild("ch", ImVec2(0, -reserveHeight), true,
             (flags_ & ImGuiFileBrowserFlags_NoModal) ? ImGuiWindowFlags_AlwaysHorizontalScrollbar : 0);
