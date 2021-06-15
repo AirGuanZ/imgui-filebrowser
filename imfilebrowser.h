c067635f3da50526cb2961b287827a479b5abe25
Alexandr <44836865+RedBull4@users.noreply.github.com>
2021-06-15 17:33:27 +0400

    Update imfilebrowser.h

--- imfilebrowser.h
+++ imfilebrowser.h
@@ -685,3 +685,37 @@ inline std::string ImGui::FileBrowser::u8StrToStr(std::u8string s) {
 #endif
 
 inline std::string ImGui::FileBrowser::u8StrToStr(std::string s) { return s; }
+
+#ifdef _WIN32
+
+	#ifndef _INC_WINDOWS
+
+		#ifndef WIN32_LEAN_AND_MEAN
+
+			#define IMGUI_FILEBROWSER_UNDEF_WIN32_LEAN_AND_MEAN
+			#define WIN32_LEAN_AND_MEAN
+
+		#endif	// #ifndef WIN32_LEAN_AND_MEAN
+
+		#include <windows.h>
+
+		#ifdef IMGUI_FILEBROWSER_UNDEF_WIN32_LEAN_AND_MEAN
+			#undef IMGUI_FILEBROWSER_UNDEF_WIN32_LEAN_AND_MEAN
+			#undef WIN32_LEAN_AND_MEAN
+		#endif	// #ifdef IMGUI_FILEBROWSER_UNDEF_WIN32_LEAN_AND_MEAN
+
+	#endif	// #ifdef _INC_WINDOWS
+
+inline std::uint32_t ImGui::FileBrowser::GetDrivesBitMask() {
+	DWORD mask = GetLogicalDrives();
+	uint32_t ret = 0;
+	for (int i = 0; i < 26; ++i) {
+		if (!(mask & (1 << i))) continue;
+		char rootName[4] = {static_cast<char>('A' + i), ':', '\\', '\0'};
+		UINT type = GetDriveTypeA(rootName);
+		if (type == DRIVE_REMOVABLE || type == DRIVE_FIXED) ret |= (1 << i);
+	}
+	return ret;
+}
+
+#endif
