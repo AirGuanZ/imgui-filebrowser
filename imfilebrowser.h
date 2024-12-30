9b072d468f0c5f31fc523d59c1186a0955356801
MaikelChan <maikelchan88@gmail.com>
2024-12-30 01:20:24 +0100

    Fixed error due to using deprecated enum.
    `ImGuiSelectableFlags_DontClosePopups` was deprecated on ImGui 1.91, and if `IMGUI_DISABLE_OBSOLETE_FUNCTIONS` is defined, it will not compile. It was renamed to `ImGuiSelectableFlags_NoAutoClosePopups`.

--- imfilebrowser.h
+++ imfilebrowser.h
@@ -633,7 +633,7 @@ inline void ImGui::FileBrowser::Display()
             }
 
             const bool selected = selectedFilenames_.find(rsc.name) != selectedFilenames_.end();
-            if(Selectable(rsc.showName.c_str(), selected, ImGuiSelectableFlags_DontClosePopups))
+            if(Selectable(rsc.showName.c_str(), selected, ImGuiSelectableFlags_NoAutoClosePopups))
             {
                 const bool wantDir = flags_ & ImGuiFileBrowserFlags_SelectDirectory;
                 const bool canSelect = rsc.name != ".." && rsc.isDir == wantDir;
